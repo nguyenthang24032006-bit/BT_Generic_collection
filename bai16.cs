bbd9c21e3d2b53330e405380458840ab2dc0cb4d
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:28:09 +0700

    bai16

--- bai16.cs
+++ bai16.cs
@@ -10,6 +10,23 @@ namespace bai16
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập chuỗi: ");
+            string str = Console.ReadLine();
+
+            Dictionary<char, int> count = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (count.ContainsKey(c))
+                    count[c]++;
+                else
+                    count[c] = 1;
+            }
+
+            foreach (var item in count)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
         }
     }
 }
