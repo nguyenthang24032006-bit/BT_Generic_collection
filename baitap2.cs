f434bab3dd9e7f6b4667562bc395cd8c61e571bc
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 21:15:44 +0700

    bai2

--- baitap2.cs
+++ baitap2.cs
@@ -11,6 +11,21 @@ namespace baitap2
         static void Main(string[] args)
         {
             Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Nhập 5 số:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine("Tổng = " + sum);
         }
     }
 }
