8026fc6fe452ba6f9bcd98034a0841606e6cea46
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 20:50:38 +0700

    bai1

---
+++ baitap1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        
+        List<int> numbers = new List<int>();
+
+        // Thêm 5 số bất kỳ
+        numbers.Add(10);
+        numbers.Add(20);
+        numbers.Add(30);
+        numbers.Add(40);
+        numbers.Add(50);
+
+        // In các phần tử ra màn hình
+     
+    }
+}
