06b9b2db674c4558262d7cca558b44272dcfb6dc
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:33:27 +0700

    bai17

--- bai17.cs
+++ bai17.cs
@@ -10,6 +10,31 @@ namespace bai17
     {
         static void Main(string[] args)
         {
+            // Ví dụ khai báo một mảng số nguyên
+            int[] numbers = { 1, 2, 3, 2, 4, 2, 5, 3 };
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+
+            foreach (int num in numbers)
+            {
+                if (freq.ContainsKey(num))
+                    freq[num]++;
+                else
+                    freq[num] = 1;
+            }
+
+            int maxCount = 0;
+            int result = numbers[0];
+
+            foreach (var item in freq)
+            {
+                if (item.Value > maxCount)
+                {
+                    maxCount = item.Value;
+                    result = item.Key;
+                }
+            }
+
+            Console.WriteLine("Số xuất hiện nhiều nhất: " + result);
         }
     }
 }
