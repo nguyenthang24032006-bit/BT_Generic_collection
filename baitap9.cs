dc1b80ffcad74ea402ace3fdead350c8e0d8b50b
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:03:51 +0700

    bai9

--- baitap9.cs
+++ baitap9.cs
@@ -10,6 +10,29 @@ namespace baitap9
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Nhập 5 số:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            List<int> uniqueList = new List<int>();
+
+            foreach (int num in numbers)
+            {
+                if (!uniqueList.Contains(num))
+                    uniqueList.Add(num);
+            }
+
+            Console.WriteLine("Danh sách không trùng:");
+            foreach (int num in uniqueList)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
