967eb8aa96821b4f13eaa4bd9ba4b8e18e64a916
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 21:43:54 +0700

    bai6

---
+++ baitap6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyen Dang Thang-2415053122339");
+        }
+    }
+}
