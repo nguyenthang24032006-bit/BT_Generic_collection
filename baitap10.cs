8fb564cc06849a1007e6fea181da366602e434af
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:07:12 +0700

    bai10

--- baitap10.cs
+++ baitap10.cs
@@ -10,6 +10,25 @@ namespace baitap10
     {
         static void Main(string[] args)
         {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Nhập 5 số:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            int min = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num < min)
+                    min = num;
+            }
+
+            Console.WriteLine("Số nhỏ nhất: " + min);
         }
     }
 }
