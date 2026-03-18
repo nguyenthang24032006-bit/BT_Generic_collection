45eba471df649a07b5d908a90337044873738638
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:25:28 +0700

    bai15

--- bai15.cs
+++ bai15.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace bai15
 {
+    class Student
+    {
+        public int Id;
+        public string Name;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("NguyenDangthang-2415053122339");
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            dict.Add("hello", "xin chào");
+            dict.Add("book", "quyển sách");
+            dict.Add("cat", "con mèo");
+
+            Console.Write("Nhập từ tiếng Anh: ");
+            string eng = Console.ReadLine();
+
+            if (dict.ContainsKey(eng))
+                Console.WriteLine("Nghĩa: " + dict[eng]);
+            else
+                Console.WriteLine("Không tìm thấy!");
         }
     }
 }
