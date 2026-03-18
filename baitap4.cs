d116a3db9b96551551c3715b364c827a65f9a4ec
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 21:36:37 +0700

    bai4

--- baitap4.cs
+++ baitap4.cs
@@ -11,6 +11,22 @@ namespace baitap4
         static void Main(string[] args)
         {
             Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            int count = 0;
+
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Nhap 5 so:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                    count++;
+            }
+
+            Console.WriteLine("Số chẵn: " + count);
         }
     }
 }
