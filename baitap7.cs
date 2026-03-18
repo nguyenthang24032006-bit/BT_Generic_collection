34633779e9719824ffa654d713155773577f2846
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 21:55:36 +0700

    bai7

--- baitap7.cs
+++ baitap7.cs
@@ -10,6 +10,23 @@ namespace baitap7
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Nhập 5 số:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            numbers.Sort();
+
+            Console.WriteLine("Danh sách tăng dần:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
