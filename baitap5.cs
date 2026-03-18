b78a8569cff25e9124fb5262a5107aae6e05ae81
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 21:41:51 +0700

    baitap5

--- baitap5.cs
+++ baitap5.cs
@@ -10,6 +10,20 @@ namespace baitap5
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            List<string> names = new List<string>() { "An", "Bình", "Cường", "Dung" };
+
+            Console.Write("Nhập tên cần xóa: ");
+            string name = Console.ReadLine();
+
+            names.Remove(name);
+
+            Console.WriteLine("Danh sách còn lại:");
+            foreach (string n in names)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
