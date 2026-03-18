0170655d151afcb1b06f70acf198ef18f8aa180d
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 21:59:11 +0700

    bai8

--- bai8.cs
+++ bai8.cs
@@ -9,7 +9,25 @@ namespace baitap8
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nguyen Dang Thang-2415053122339");
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Nhập 5 số:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            numbers.Reverse();
+
+            Console.WriteLine("Danh sách đảo ngược:");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
