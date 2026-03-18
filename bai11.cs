f5fb440edce2cc1a21de099225942e3049086570
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:11:30 +0700

    bai11

--- bai11.cs
+++ bai11.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace bai11
 {
+    class Student
+    {
+        public int Id;
+        public string Name;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Student> list = new List<Student>();
+
+            list.Add(new Student { Id = 1, Name = "An" });
+            list.Add(new Student { Id = 2, Name = "Binh" });
+            list.Add(new Student { Id = 3, Name = "Cuong" });
+
+            Console.WriteLine("Danh sách sinh viên:");
+            foreach (Student s in list)
+            {
+                Console.WriteLine($"ID: {s.Id}, Name: {s.Name}");
+            }
         }
     }
 }
