59dc09852e7e8a46db52f0be1f3d6a6e7fb428ce
nguyenthang24032006-bit <nguyenthang24032006@gmail.com>
2026-03-18 22:19:13 +0700

    bai13

--- bai13.cs
+++ bai13.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace bai13
 {
+    class Student
+    {
+        public int Id;
+        public string Name;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("NguyenDangthang-2415053122339");
+            List<Student> list = new List<Student>();
+
+            list.Add(new Student { Id = 1, Name = "An" });
+            list.Add(new Student { Id = 2, Name = "Binh" });
+            list.Add(new Student { Id = 3, Name = "Cuong" });
+
+            Console.WriteLine("Số sinh viên: " + list.Count);
         }
     }
 }
